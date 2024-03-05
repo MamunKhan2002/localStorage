158576c33d1c819a9852c5aa91c614f95944c0eb
Mamun <mamunkhan2002bd@gmail.com>
2024-03-05 14:49:39 +0600

    My first commit

---
+++ local-storage.js
@@ -0,0 +1,9 @@
+// Handle add card btn here
+const addCard = () => {
+    const productField = document.getElementById('product_field');
+    const quantityField = document.getElementById('quantity_field');
+    const productFieldValue = productField.value;
+    const quantityFieldValue = quantityField.value;
+
+    console.log(productFieldValue, quantityFieldValue);
+}
